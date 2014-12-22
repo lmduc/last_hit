9fb6405d986ce38d48816f4e22a6bcf78beb4e67
DUC LE <ducle@DUCs-MacBook-Pro-2.local>
2014-12-23 00:35:48 +0700

    Try to make gem work 1

--- lib/last_hit.rb
+++ lib/last_hit.rb
@@ -1,12 +1,16 @@
 require "last_hit/version"
 require 'last_hit/railtie' if defined?(Rails::Railtie)
-
-require 'last_hit/rc_adapter/git_adapter'
-require 'last_hit/file_filter/spec_filter'
-
 require 'last_hit/test_handler'
 
 module LastHit
+  module RcAdapter
+    require 'last_hit/rc_adapter/git_adapter'
+  end
+
+  module FileFilter
+    require 'last_hit/file_filter/spec_filter'
+  end
+
   class << self
     def run_modified_tests
       files = RcAdapter::GitAdapter.get_modified_files
