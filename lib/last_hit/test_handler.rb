6b90b4e272b717a33cd40687b1914dd87022d3b0
DUC LE <ducle@DUCs-MacBook-Pro-2.local>
2014-12-26 01:01:19 +0700

    Fix bug that no tests found

--- lib/last_hit/test_handler.rb
+++ lib/last_hit/test_handler.rb
@@ -2,14 +2,26 @@ module LastHit
   class TestHandler
     class << self
       def run(files)
+        if files.empty?
+          run_no_tests
+        else
+          run_tests(files)
+        end
+      end
+
+      private
+
+      def run_no_tests
+        $stdout.puts "No test found"
+      end
+
+      def run_tests(files)
         file_string = files.join(" ")
 
         before_run(file_string)
         exec_command = command(file_string)
         system(exec_command)
       end
-
-      private
 
       def before_run(file_string)
         $stdout.puts "Run files: #{file_string}"
