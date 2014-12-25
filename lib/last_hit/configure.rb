9ea90ab603ec6359909d57da88ae060b22788898
DUC LE <ducle@DUCs-MacBook-Pro-2.local>
2014-12-26 00:28:07 +0700

    Default values for config

--- lib/last_hit/configure.rb
+++ lib/last_hit/configure.rb
@@ -1,5 +1,8 @@
 module LastHit
   class Configure
+    @@test_command = "bundle exec rspec"
+    @@default_base_branch = "development"
+
     class << self
       attr_accessor :test_command, :default_base_branch
 
