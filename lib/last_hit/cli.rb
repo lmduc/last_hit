25dfdb73da5f17b95c691807332e8059bb956714
MatBi <leminhducktvn@gmail.com>
2016-04-10 03:04:20 +0800

    Fix a bug that cannot run the last_hit command

--- lib/last_hit/cli.rb
+++ lib/last_hit/cli.rb
@@ -6,13 +6,13 @@ class LastHit
 
     desc 'modified_tests', 'Run tests which are currently modified'
     def modified_tests
-      LastHit.modified_tests
+      LastHit.new.modified_tests
     end
 
     desc 'all_tests', 'Run all tests which have been modified in the current branch'
     method_option :base, aliases: '-b', desc: 'Base branch for compare', type: :string
     def all_tests
-      LastHit.all_tests(options[:base])
+      LastHit.new.all_tests(options[:base])
     end
   end
 end
