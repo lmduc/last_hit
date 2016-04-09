5f5606bdd960b69a5c9126c510da30f3282e03ba
MatBi <leminhducktvn@gmail.com>
2016-04-10 01:19:22 +0800

    Change the way of config

--- lib/last_hit/configure.rb
+++ lib/last_hit/configure.rb
@@ -1,14 +1,14 @@
-module LastHit
+class LastHit
+  def self.config
+    yield(Configure) if block_given?
+  end
+
   class Configure
     @test_command = "bundle exec rspec"
     @default_base_branch = "development"
 
     class << self
       attr_accessor :test_command, :default_base_branch
-
-      def config
-        yield(self)
-      end
     end
   end
 end
