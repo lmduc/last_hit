415a339a3aeb452c5d084f15801b8e57f7ce5bd4
MatBi <leminhducktvn@gmail.com>
2016-04-10 02:43:34 +0800

    Cli class

---
+++ lib/last_hit/cli.rb
@@ -0,0 +1,18 @@
+require 'thor'
+
+class LastHit
+  class Cli < Thor
+    include Thor::Actions
+
+    desc 'modified_tests', 'Run tests which are currently modified'
+    def modified_tests
+      LastHit.modified_tests
+    end
+
+    desc 'all_tests', 'Run all tests which have been modified in the current branch'
+    method_option :base, aliases: '-b', desc: 'Base branch for compare', type: :string
+    def all_tests
+      LastHit.all_tests(options[:base])
+    end
+  end
+end
